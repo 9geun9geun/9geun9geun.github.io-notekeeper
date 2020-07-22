0828ea74079c1ff46efe0650a663edbd394bf35f
Keun Mo <rnrmsah.koo@gmail.com>
2020-07-22 23:22:48 +1200

    deploy react app

---
+++ build/precache-manifest.55ecdf9082e2ee482bc9fc319e8112ee.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78e3c3506ef850f865013ae91c26e58d",
+    "url": "/React-Keeper-Note-App/index.html"
+  },
+  {
+    "revision": "499a1e7351c830159f13",
+    "url": "/React-Keeper-Note-App/static/js/2.f5de51ba.chunk.js"
+  },
+  {
+    "revision": "0236adf05bbd1148b46c",
+    "url": "/React-Keeper-Note-App/static/js/main.d3e5737a.chunk.js"
+  },
+  {
+    "revision": "79376b9319fb3c110440",
+    "url": "/React-Keeper-Note-App/static/js/runtime-main.661f526f.js"
+  }
+]);
